6a3300db2c997fc248058315b18f5c9b8e5336b1
bobby <bobbyisbest3@gmail.com>
2018-11-30 11:03:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import numpy as np
 from A3 import KLR, SKLR
 
 
-RUNS = 25
+RUNS = 20
 
 metrics = np.zeros(7)
 for _ in range(RUNS):
-    np.add(metrics, SKLR.main(sys.argv))
-np.divide(metrics, RUNS)
+    metrics = np.add(metrics, SKLR.main(sys.argv))
+metrics = np.divide(metrics, RUNS)
 
 print(metrics)
